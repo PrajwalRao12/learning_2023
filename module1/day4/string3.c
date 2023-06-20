a5aef80cb79c5d5fa3aa54dbfed35bcbf2fd40a0
PrajwalRao12 <prajwalrao12122001@gmail.com>
2023-06-20 16:49:11 +0530

    day five activities

--- module1/day4/string3.c
+++ module1/day4/string3.c
@@ -1,33 +1,16 @@
-#include <stdio.h>
-#include <stdlib.h>
-#include <string.h>
-
-int calculateTotalSeconds(char *timeStr) {
-    char *token;
-    int hours, minutes, seconds;
-    
-    token = strtok(timeStr, ":");
-    hours = strtoul(token, NULL, 10);
-    
-    token = strtok(NULL, ":");
-    minutes = strtoul(token, NULL, 10);
-    
-    token = strtok(NULL, ":");
-    seconds = strtoul(token, NULL, 10);
-
-    int totalSeconds = hours * 3600 + minutes * 60 + seconds;
-    return totalSeconds;
-}
-
-int main() {
-    char timeStr[9];
-
-    printf("Enter the time in hh:mm:ss format: ");
-    fgets(timeStr, sizeof(timeStr), stdin);
-    timeStr[strcspn(timeStr, "\n")] = '\0'; 
-    int totalSeconds = calculateTotalSeconds(timeStr);
-
-    printf("Total seconds: %d\n", totalSeconds);
-
-    return 0;
-}
+  #include <stdio.h>
+    struct p
+    {
+        int x[2];
+    };
+    struct q
+    {
+        int *x;
+    };
+    int main()
+    {
+        struct p p1 = {1, 2};
+        struct q *ptr1;
+        ptr1->x = (struct q*)&p1.x;
+        printf("%d\n", ptr1->x[1]);
+    }
